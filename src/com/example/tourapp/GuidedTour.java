43a5ae3389c6a2d777db93ddbeeb4e1c2179f050
nwashington <nicole.adero@gmail.com>
2011-04-18 18:00:29 -0700

    Created the MapView and Itemized Overlay. Application keeps crashing now.

--- src/com/example/tourapp/GuidedTour.java
+++ src/com/example/tourapp/GuidedTour.java
@@ -1,8 +1,15 @@
 package com.example.tourapp;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class GuidedTour extends MapActivity {
 
@@ -10,7 +17,19 @@ public class GuidedTour extends MapActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.guidedtour);
+		 MapView mapView = (MapView) findViewById(R.id.mapview);
+		    mapView.setBuiltInZoomControls(true);
+		    
+		    List<Overlay> mapOverlays = mapView.getOverlays();
+		    Drawable drawable = this.getResources().getDrawable(R.drawable.juxb1kxa);
+		    HelloItemizedOverlay itemizedoverlay = new HelloItemizedOverlay(drawable);
+		    
+		    GeoPoint point = new GeoPoint(19240000,-99120000);
+		    OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+		    itemizedoverlay.addOverlay(overlayitem);
+		    mapOverlays.add(itemizedoverlay);
 	}
+	
 
 	@Override
 	protected boolean isRouteDisplayed() {
