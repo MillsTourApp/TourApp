42238692ec99ae5d3b3cb6cef514deb42162ec03
Anna <anna@fornaeus.com>
2011-04-30 14:16:29 -0700

    adding text to underneath map on GuidedTour.java

--- src/com/example/tourapp/GuidedTour.java
+++ src/com/example/tourapp/GuidedTour.java
@@ -1,19 +1,13 @@
 package com.example.tourapp;
 
-import java.util.List;
-
-import android.graphics.drawable.Drawable;
+import java.util.ArrayList;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
-import com.google.android.maps.OverlayItem;
-import com.google.android.maps.MapView.LayoutParams;
 
 public class GuidedTour extends MapActivity {
 	
@@ -23,6 +17,8 @@ public class GuidedTour extends MapActivity {
 
     static final String MILLS_HALL_LAT = "37.77946909500844";
     static final String MILLS_HALL_LONG = "-122.18220055103302";
+    
+    TextView descView;
  
     /** Called when the activity is first created. */
     @Override
@@ -31,7 +27,7 @@ public class GuidedTour extends MapActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guidedtour);
         
- 
+        //map stuff
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
  
@@ -48,6 +44,32 @@ public class GuidedTour extends MapActivity {
         mc.setZoom(18);
         mapView.setSatellite(true);
         mapView.invalidate();
+        
+        
+        //ArrayList test stuff
+        ArrayList<DivisionObject> manualListOfDivisionObjects = new ArrayList<DivisionObject>(30);
+        
+        DivisionObject obj1 = new DivisionObject(1, "Mills Hall", "4", "5", "This is a description of Mills Hall","These are directions to the building before Mills Hall", "This is the name of an image for Mills Hall");
+        manualListOfDivisionObjects.add(obj1);
+        DivisionObject obj2 = new DivisionObject(1, "Natural Sciences Building", "4", "5", "This is a description of Natural Sciences Building","These are directions to the building before the Natural Sciences Building", "This is the name of an image for the Natural Sciences Building");
+        manualListOfDivisionObjects.add(obj2);
+        DivisionObject obj3 = new DivisionObject(1, "Carnegie Hall", "4", "5", "This is a description of Carnegie Hall","These are directions to the building before Carnegie Hall", "This is the name of an image for Carnegie Hall");
+        manualListOfDivisionObjects.add(obj3);
+        DivisionObject obj4 = new DivisionObject(1, "CPM", "4", "5", "This is a description of CPM","These are directions to the building before CPM", "This is the name of an image for CPM");
+        manualListOfDivisionObjects.add(obj4);
+       
+        String testManualArray = "";
+        CharSequence testMArray2 = "";
+        
+        
+        for (DivisionObject obj:manualListOfDivisionObjects){
+            testManualArray+= obj.getName() + "\n";
+        }
+        
+        
+        
+        descView = (TextView) findViewById(R.id.text_view);
+        descView.setText(testManualArray);
     }
 	
     @Override
