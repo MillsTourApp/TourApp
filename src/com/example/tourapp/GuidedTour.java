7de5fe63ebf1231a8a0bf34c7532ee79de6d1876
Anna <anna@fornaeus.com>
2011-05-01 16:50:06 -0700

    merged Shannon's and Anna's maps, added comments, and discarded magic numbers.

--- src/com/example/tourapp/GuidedTour.java
+++ src/com/example/tourapp/GuidedTour.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -18,69 +19,94 @@ import com.google.android.maps.OverlayItem;
 import com.google.android.maps.MapView.LayoutParams;
 
 public class GuidedTour extends MapActivity {
+	//map fields
+	private MapView mMapView;
+	private List<Overlay> mMapOverlays;
+	private MapController mController;
+	private Drawable mDrawable;
+	private MyItemizedOverlay mItemizedOverlay;
 	
-	private MapView mapView;
-	private List<Overlay> mapOverlays;
-	private MapController controller;
-	private Drawable drawable;
-	private MyItemizedOverlay itemizedOverlay;
-	//test arraylist
-	private ArrayList<DivisionObject> manualListOfDivisionObjects;
+	//text fields
+	private TextView titleView;
+	private TextView descView;
 
+	//constants
+	private static final int NUM_PLACE_OBJ = 30; //temp
+	private static final int BALLOON_OPACITY = 200;
+	private static final int MAP_ZOOM = 18;
+	private static final double NORMALIZE_COORDINATES = 1E6;
+	
+	//test arraylist
+	private ArrayList<PlaceObject> mManualListOfPlaceObjects;
+
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-/*		//create the array and fill it
-		manualListOfDivisionObjects = new ArrayList<DivisionObject>(30);
-		DivisionObject obj1 = new DivisionObject(0, "Mills Hall", "-122.18212", "37.779496", "This is a description of Mills Hall.","Start your tour here!", "This is the name of an image for Mills Hall");
-		manualListOfDivisionObjects.add(obj1);
-		DivisionObject obj2 = new DivisionObject(1, "Natural Sciences Building", "-122.180666", "37.780143", "This is a description of Natural Sciences Building","Directions from Mills Hall: These are directions to the building before the Natural Sciences Building", "This is the name of an image for the Natural Sciences Building");
-		manualListOfDivisionObjects.add(obj2);
-		DivisionObject obj3 = new DivisionObject(2, "Carnegie Hall", "-122.181645", "37.778995", "This is a description of Carnegie Hall","Directions from NSB: These are directions from the building before Carnegie Hall", "This is the name of an image for Carnegie Hall");
-		manualListOfDivisionObjects.add(obj3);
-		DivisionObject obj4 = new DivisionObject(3, "CPM", "-122.182791", "37.778726", "This is a description of CPM","From Carnegie Hall: These are directions from the building before CPM", "This is the name of an image for CPM");
-		manualListOfDivisionObjects.add(obj4);*/
-
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.map_layout);
+		setContentView(R.layout.guidedtour);
+		
+		//temporary testing stuff
+		mManualListOfPlaceObjects = new ArrayList<PlaceObject>(NUM_PLACE_OBJ);
+		PlaceObject obj1 = new PlaceObject(0, "Mills Hall", "-122.18212", "37.779496", "This is a description of Mills Hall.","Start your tour here!", "this is a description of where you came from", "This is the name of an image for Mills Hall");
+		mManualListOfPlaceObjects.add(obj1);
+		PlaceObject obj2 = new PlaceObject(1, "Natural Sciences Building", "-122.180666", "37.780143", "This is a description of Natural Sciences Building","Directions from Mills Hall: These are directions to the building before the Natural Sciences Building", "this is a description of where you came from", "This is the name of an image for the Natural Sciences Building");
+		mManualListOfPlaceObjects.add(obj2);
+		PlaceObject obj3 = new PlaceObject(2, "Carnegie Hall", "-122.181645", "37.778995", "This is a description of Carnegie Hall","Directions from NSB: These are directions from the building before Carnegie Hall", "this is a description of where you came from", "This is the name of an image for Carnegie Hall");
+		mManualListOfPlaceObjects.add(obj3);
+		PlaceObject obj4 = new PlaceObject(3, "CPM", "-122.182791", "37.778726", "This is a description of CPM","From Carnegie Hall: These are directions from the building before CPM", "this is a description of where you came from", "This is the name of an image for CPM");
+		mManualListOfPlaceObjects.add(obj4);
+
+		PlaceObject currentPlace = obj1; //set to Mills Hall for testing purposes
+		
+		//map view
 		initMapView();
 		initMyLocation();
-		mapOverlays = mapView.getOverlays();
+		mMapOverlays = mMapView.getOverlays();
+		
 		// create itemized overlay
-		drawable = getResources().getDrawable(R.drawable.marker);
-		drawable.setAlpha(200);
-		itemizedOverlay = new MyItemizedOverlay(drawable, mapView);
+		mDrawable = getResources().getDrawable(R.drawable.marker);
+		mDrawable.setAlpha(BALLOON_OPACITY);
+		mItemizedOverlay = new MyItemizedOverlay(mDrawable, mMapView);
+		
 		//Create balloon overlays using test arraylist
-/*		for (DivisionObject obj:manualListOfDivisionObjects){
+		for (PlaceObject obj : mManualListOfPlaceObjects){
 			double lat = Double.parseDouble(obj.getLat());
 			double lon = Double.parseDouble(obj.getLon());
 			createOverlay(lat, lon, obj.getId(), obj.getName(), obj.getDirFromPrev());
-		}*/
-		mapOverlays.add(itemizedOverlay);
+		} //for
+		mMapOverlays.add(mItemizedOverlay);
 		
-	}
+		//view stuff		
+		titleView = (TextView) findViewById(R.id.title_view);
+		titleView.setText(currentPlace.getName());
+		
+		descView = (TextView) findViewById(R.id.desc_view);
+		descView.setText(currentPlace.getDescription());
+	} //onCreate
 	
+	//helper methods
 	/** Find and initialize the map view. (From Hello Android tutorial)*/
 	private void initMapView() {
-		mapView = (MapView) findViewById(R.id.mapView2);
-		controller = mapView.getController();
-		mapView.setSatellite(true);
-		mapView.setBuiltInZoomControls(true);
-	}
+		mMapView = (MapView) findViewById(R.id.mapview);
+		mController = mMapView.getController();
+		mMapView.setSatellite(true);
+		mMapView.setBuiltInZoomControls(true);
+	} //initMapView
 
 	/** Start tracking the position on the map. (From Hello Android tutorial)*/
 	private void initMyLocation() {
-		final MyLocationOverlay overlay = new MyLocationOverlay(this, mapView);
+		final MyLocationOverlay overlay = new MyLocationOverlay(this, mMapView);
 		overlay.enableMyLocation();
 		overlay.enableCompass(); // does not work in emulator
 		overlay.runOnFirstFix(new Runnable() {
 			public void run() {
 				// Zoom in to current location
-				controller.setZoom(17);
-				controller.animateTo(overlay.getMyLocation());
-			}
-		});
-		mapView.getOverlays().add(overlay);
-	}
+				mController.setZoom(MAP_ZOOM);
+				mController.animateTo(overlay.getMyLocation());
+			} //run
+		}); //runOnFirstFix
+		mMapView.getOverlays().add(overlay);
+	} //initMyLocation
 
 	/**
 	 * Creates a balloon overlay that puts a point on the map of the building specified 
@@ -91,17 +117,17 @@ public class GuidedTour extends MapActivity {
 	 * @param directions Directions to the building from the previous building
 	 */
 	public void createOverlay (double lat, double lon, int id, String name, String directions){
-		GeoPoint point = new GeoPoint((int)(lat*1E6),(int)(lon*1E6));
+		GeoPoint point = new GeoPoint((int)(lat*NORMALIZE_COORDINATES ),(int)(lon*NORMALIZE_COORDINATES));
 		id++; //So id starts at 1, not 0
 		OverlayItem overlayItem = new OverlayItem(point, id + ". "+ name, directions);
-		itemizedOverlay.addOverlay(overlayItem);
-	}
+		mItemizedOverlay.addOverlay(overlayItem);
+	} //createOverlay
 
 	public String getTextOfSelectedBalloon(int index){
-		DivisionObject obj = manualListOfDivisionObjects.get(index);
-		return obj.getDescription();
-		
-	}
+		PlaceObject obj = mManualListOfPlaceObjects.get(index);
+		return obj.getDescription();	
+	} //getTextOfSelectedBalloon
+	
 	/**
 	 * Specifies whether there is a route displayed
 	 * @return True is a route is displayed, false if there is not a route displayed
@@ -109,5 +135,5 @@ public class GuidedTour extends MapActivity {
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
-	}
-}
+	} //isRouteDisplayed
+} //GuidedTour
