97f07427f7171096df38d63fccd4d7d17bbeb150
sthompso <sthompso@mills.edu>
2011-05-01 11:27:49 -0700

    BalloonItemizedOverlay, BalloonOverlayView, HelloItemizedOverlay, MyItemizedOverlay were added.

---
+++ src/com/example/tourapp/MyItemizedOverlay.java
@@ -0,0 +1,60 @@
+package com.example.tourapp;
+
+/***
+ * Copyright (c) 2010 readyState Software Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+
+public class MyItemizedOverlay extends BalloonItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> m_overlays = new ArrayList<OverlayItem>();
+	private Context c;
+	
+	public MyItemizedOverlay(Drawable defaultMarker, MapView mapView) {
+		super(boundCenter(defaultMarker), mapView);
+		c = mapView.getContext();
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+	    m_overlays.add(overlay);
+	    populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return m_overlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return m_overlays.size();
+	}
+
+	/*@Override
+	protected boolean onBalloonTap(int index, OverlayItem item) {
+		index++; //so that location number 1 is 1 rather than 0 on the tour
+		Toast.makeText(c, "Destination number " + index + " on your guided tour!",
+				Toast.LENGTH_LONG).show();
+		return true;
+	}*/
+	
+}
