a9dae1a1a073bfd12c55c74ea8ac98ed370d6c3e
Anna <anna@fornaeus.com>
2011-05-01 11:45:11 -0700

    Map works with pulling coordinates out of DivisionObject!

--- src/com/example/tourapp/GuidedTour.java
+++ src/com/example/tourapp/GuidedTour.java
@@ -19,11 +19,6 @@ public class GuidedTour extends MapActivity {
 	TextView titleView;
 	TextView descView;
 	
-	//misc fields
-	static final String MILLS_HALL_LAT = "37.77946909500844";
-	static final String MILLS_HALL_LONG = "-122.18220055103302";
-
-
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -34,7 +29,7 @@ public class GuidedTour extends MapActivity {
 		//ArrayList test stuff
 		ArrayList<DivisionObject> manualListOfDivisionObjects = new ArrayList<DivisionObject>(30);
 
-		DivisionObject obj1 = new DivisionObject(1, "Mills Hall", "4", "5", "This is a description of Mills Hall","These are directions to the building before Mills Hall", "This is the name of an image for Mills Hall");
+		DivisionObject obj1 = new DivisionObject(1, "Mills Hall", "-122.18220055103302", "37.77946909500844", "This is a description of Mills Hall","These are directions to the building before Mills Hall", "This is the name of an image for Mills Hall");
 		manualListOfDivisionObjects.add(obj1);
 		DivisionObject obj2 = new DivisionObject(1, "Natural Sciences Building", "4", "5", "This is a description of Natural Sciences Building","These are directions to the building before the Natural Sciences Building", "This is the name of an image for the Natural Sciences Building");
 		manualListOfDivisionObjects.add(obj2);
@@ -43,31 +38,32 @@ public class GuidedTour extends MapActivity {
 		DivisionObject obj4 = new DivisionObject(1, "CPM", "4", "5", "This is a description of CPM","These are directions to the building before CPM", "This is the name of an image for CPM");
 		manualListOfDivisionObjects.add(obj4);
 
+		DivisionObject currentPlace = obj1; //set to Mills Hall for testing purposes
 		
 		//map stuff
 		mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
 
 		mc = mapView.getController();
-		String coordinates[] = {MILLS_HALL_LAT, MILLS_HALL_LONG};
-		double lat = Double.parseDouble(coordinates[0]);
-		double lng = Double.parseDouble(coordinates[1]);
+		String coordinates[] = {currentPlace.getLat(), currentPlace.getLon()};
+		double lng = Double.parseDouble(coordinates[0]);
+		double lat = Double.parseDouble(coordinates[1]);
 
 		p = new GeoPoint(
-				(int) (lat * 1E6),
-				(int) (lng * 1E6));
+				(int) (lng * 1E6),
+				(int) (lat * 1E6));
 
 		mc.animateTo(p);
 		mc.setZoom(18);
 		mapView.setSatellite(true);
 		mapView.invalidate();
 
-		//view stuff
+		//view stuff		
 		titleView = (TextView) findViewById(R.id.title_view);
-		titleView.setText(obj2.getName());
+		titleView.setText(currentPlace.getName());
 		
 		descView = (TextView) findViewById(R.id.desc_view);
-		descView.setText(obj2.getDescription());
+		descView.setText(currentPlace.getDescription());
 		
 	}
 
