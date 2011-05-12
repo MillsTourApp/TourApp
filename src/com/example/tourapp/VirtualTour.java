127ba12340a91c9202f61c5b5a04ca59d533c30f
sthompso <sthompso@mills.edu>
2011-05-11 20:22:57 -0700

    Nicole's comment changes to VirtualTour (she had problems pushing to Git)

--- src/com/example/tourapp/VirtualTour.java
+++ src/com/example/tourapp/VirtualTour.java
@@ -13,19 +13,41 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+/**
+ * The start of the virtual tour class, creating a grid view, extending an activity
+ * @author nwashington
+ * @author sthompson
+ *
+ */
 public class VirtualTour extends Activity{
+	/**
+	 * recognizing the array list called PlaceObject, naming it as listOfPlaces
+	 */
 	public ArrayList<PlaceObject> listOfPlaces;
-	
+	/**
+	 * onCreate Method, creating the virtual tour grid
+	 *@param savedInstanceState
+	 */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.virtualtourgrid);
-		
+
+		//Obtaining data from intent passed in last activity
 		Bundle extras = getIntent().getExtras();
 		listOfPlaces = extras.getParcelableArrayList("com.example.tourapp.placeArrayList");
-	
+
+		//the grid is now captured from layout with findViewById
 		GridView gridview = (GridView) findViewById(R.id.virtualtourgrid);
-		gridview.setAdapter(new ImageAdapter(this));
-		gridview.setOnItemClickListener(new OnItemClickListener() {
+		gridview.setAdapter(new ImageAdapter(this));//sets custom adapter as the source for all images provided
+		gridview.setOnItemClickListener(new OnItemClickListener() {//to do something when item is clicked.
+			/**
+			 * When the item is clicked, this takes you to the PlaceObject class which pulls the specified data(name, descrpt, imageName from the database, to get the correct info
+			 * @param position references what is clicked
+			 * @param index references what is clicked
+			 * @param id references what is clicked
+			 * @param view references what is clicked
+			 * @param parent references what is clicked
+			 */
 			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 				Object object = findObject(position);
 				PlaceObject placeObject = (PlaceObject)object;
@@ -37,30 +59,50 @@ public class VirtualTour extends Activity{
 				intent.putExtra("description", description);
 				intent.putExtra("imageName", imageName);
 				startActivity(intent);
-			}
+			}//onItemClick
 		});
-	}
+	}//onCreate
 
+	/**
+	 * implements required methods from BaseAdapter
+	 */
 	public class ImageAdapter extends BaseAdapter {
 		private Context mContext;
-
+		/**
+		 * referring to the ImageAdapter as mContext
+		 * @param c 
+		 */
 		public ImageAdapter(Context c) {
 			mContext = c;
 		}
-
+		/**
+		 * @return the size of the places
+		 */
 		public int getCount() {
 			return listOfPlaces.size();
 		}
-
+		/**
+		 * returns the actual object at the specified position in the adapter
+		 * @return null
+		 */
 		public Object getItem(int position) {
 			return null;
 		}
-
+		/**
+		 * returns the row id of the current item
+		 * @return not needed at this time.
+		 */
 		public long getItemId(int position) {
 			return 0;
 		}
 
-		// create a new ImageView for each item referenced by the Adapter
+		/**
+		 *creates a new ImageView for each item referenced by the Adapter
+		 *@param position of the image
+		 *@param convertView 
+		 *@param parent
+		 *@return imageView going to show the image
+		 */
 		public View getView(int position, View convertView, ViewGroup parent) {
 			ImageView imageView;
 			if (convertView == null) {  // if it's not recycled, initialize some attributes
@@ -70,12 +112,17 @@ public class VirtualTour extends Activity{
 				imageView.setPadding(8, 8, 8, 8);
 			} else {
 				imageView = (ImageView) convertView;
-			}
+			}//else
 			imageView.setImageResource(findImageName(position));
 			return imageView;
-		}
-	}
-
+		}//getView
+	}//Image Adapter
+	
+	/**
+	 * this will find the specific object we that has been clicked
+	 * @param position
+	 * @return the object
+	 */
 	public PlaceObject findObject(int position){
 		for (PlaceObject obj:listOfPlaces){
 			if(obj.getId()-1 == position){
@@ -83,19 +130,24 @@ public class VirtualTour extends Activity{
 			}
 		}
 		return null;
-	}
-	
+	}//findObject
+
+	/**
+	 * Finds the image associated with the index
+	 * @param position index of the desired picture
+	 * @return the image to be shown
+	 */
 	public int findImageName(int position){
 		for (PlaceObject obj:listOfPlaces){
 			if(obj.getId()-1 == position){
-				return findImage(obj);
-			}
-		}
+				return findImageName(obj);
+			}//if
+		}//for
 		return 0; //image of that position was not found
-	}
-	
-	public int findImage(PlaceObject obj){
+	}//public findImageName
+
+	private int findImageName(PlaceObject obj){
 		int resId = this.getResources().getIdentifier(obj.getImageName(), "drawable", "com.example.tourapp");
 		return resId;
-	}
-}
+	}//private findImageName
+}//VirtualTour
